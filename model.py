15034e5c7fa86eb386ec9579b0305f3176dd0de2
justas <justasbr@gmail.com>
2018-02-14 20:22:58 +0000

    trimming code

--- model.py
+++ model.py
@@ -80,7 +80,8 @@ class DCGAN(object):
             self.c_dim = 1
 
         self.grayscale = (self.c_dim == 1)
-
+        print("Y", self.y)
+        print("Y_DIM", self.y_dim)
         self.build_model()
 
     def build_model(self):
@@ -387,43 +388,6 @@ class DCGAN(object):
                 h2 = conv_cond_concat(h2, yb)
 
                 return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
-
-    # def load_mnist(self):
-    #     data_dir = os.path.join("./data", self.dataset_name)
-    #
-    #     fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
-    #     loaded = np.fromfile(file=fd, dtype=np.uint8)
-    #     trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float)
-    #
-    #     fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
-    #     loaded = np.fromfile(file=fd, dtype=np.uint8)
-    #     trY = loaded[8:].reshape((60000)).astype(np.float)
-    #
-    #     fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
-    #     loaded = np.fromfile(file=fd, dtype=np.uint8)
-    #     teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
-    #
-    #     fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
-    #     loaded = np.fromfile(file=fd, dtype=np.uint8)
-    #     teY = loaded[8:].reshape((10000)).astype(np.float)
-    #
-    #     trY = np.asarray(trY)
-    #     teY = np.asarray(teY)
-    #
-    #     X = np.concatenate((trX, teX), axis=0)
-    #     y = np.concatenate((trY, teY), axis=0).astype(np.int)
-    #
-    #     seed = 547
-    #     np.random.seed(seed)
-    #     np.random.shuffle(X)
-    #     np.random.seed(seed)
-    #     np.random.shuffle(y)
-    #
-    #     y_vec = np.zeros((len(y), self.y_dim), dtype=np.float)
-    #     for i, label in enumerate(y):
-    #         y_vec[i, y[i]] = 1.0
-    #
-    #     return X / 255., y_vec
 
     @property
     def model_dir(self):
