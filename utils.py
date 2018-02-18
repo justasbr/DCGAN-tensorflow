59205fa780453275859fafe771a060f2d7cfddfd
Mantas <M.Serapinas@gmail.com>
2018-02-18 23:57:21 +0000

    adjusted centered crop

--- utils.py
+++ utils.py
@@ -66,15 +66,13 @@ def imsave(images, size, path):
   image = np.squeeze(merge(images, size))
   return scipy.misc.imsave(path, image)
 
-def center_crop(x, crop_h, crop_w,
+def center_crop(img, crop_h, crop_w,
                 resize_h=64, resize_w=64):
-  if crop_w is None:
-    crop_w = crop_h
-  h, w = x.shape[:2]
-  j = int(round((h - crop_h)/2.))
-  i = int(round((w - crop_w)/2.))
-  return scipy.misc.imresize(
-      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
+  y, x = img.shape[:2]
+  startx = x//2 - 32
+  starty = y//2 - 32
+  return img[starty:starty+64, startx:startx+64]
+
 
 def transform(image, input_height, input_width, 
               resize_height=64, resize_width=64, crop=True):
